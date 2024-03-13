5ba2582d794d9da3738c574cab641317d0b1d036
kambhani <63814718+kambhani@users.noreply.github.com>
2024-03-12 21:21:57 -0400

    Add email functionality

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,10 +1,17 @@
 import type { APIRoute } from 'astro';
+import Mailgun from 'mailgun.js';
 
 export const POST: APIRoute = async ({ request }) => {
   const data = await request.formData();
   const name = data.get('name');
   const email = data.get('email');
   const message = data.get('message');
+
+  const mailgun = new Mailgun(FormData);
+  const mg = mailgun.client({
+    username: 'api',
+    key: import.meta.env.MAILGUN_API_KEY || 'API-KEY',
+  });
 
   console.log(name, email, message);
 
@@ -18,11 +25,29 @@ export const POST: APIRoute = async ({ request }) => {
     );
   }
 
-  // Do something with the data, then return a success response
-  return new Response(
-    JSON.stringify({
-      message: 'Success!',
-    }),
-    { status: 200 }
-  );
+  try {
+    const resp = await mg.messages.create(import.meta.env.MAILGUN_EMAIL || 'EMAIL', {
+      from: `mailgun@${import.meta.env.MAILGUN_EMAIL || 'EMAIL'}`,
+      to: 'kraghuranish@gmail.com',
+      subject: 'Hello World!',
+      text: `${name}\n${email}\n${message}\n`,
+    });
+
+    console.log(resp);
+
+    return new Response(
+      JSON.stringify({
+        message: 'Success!',
+      }),
+      { status: 200 }
+    );
+  } catch (err) {
+    console.log(err);
+    return new Response(
+      JSON.stringify({
+        message: 'Internal server error',
+      }),
+      { status: 500 }
+    );
+  }
 };

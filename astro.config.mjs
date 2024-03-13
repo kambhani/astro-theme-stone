b33ff06356b593d2828fe70d67913dc373b669b8
kambhani <63814718+kambhani@users.noreply.github.com>
2024-03-12 21:59:43 -0400

    Fix contact form

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import vercel from '@astrojs/vercel/serverless';
+import alpine from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel(),
-  integrations: [tailwind(), compress()],
+  integrations: [tailwind(), compress(), alpine()],
 });
